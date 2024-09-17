f3601ba077e60fbf1d1bed1afc76c45da9fabb3b
YoshiakiAsahara <asa-yossan@outlook.jp>
2024-09-17 14:57:16 +0900

    stacon

---
+++ app/component/styledComponent.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const styledJsx = () => {
+  return (
+    <>
+      <div className="container">
+        <p className="title">styledJsx</p>
+        <button className="button">ボタン</button>
+      </div>
+    </>
+  );
+};
+
+export default styledJsx;
